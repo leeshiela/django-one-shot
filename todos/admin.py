aac65e6757e12a382835dbe01dc6c047001c6d1d
Shiela Lee <leeshiela@gmail.com>
2023-12-12 11:30:26 -0500

    Feature 6 Complete

--- todos/admin.py
+++ todos/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from todos.models import ToDoList
+from todos.models import ToDoList, ToDoItem
 
 @admin.register(ToDoList)
 class ToDoListAdmin(admin.ModelAdmin):
     list_display = [
         "name",
         "id",
+    ]
+
+@admin.register(ToDoItem)
+class ToDoItemAdmin(admin.ModelAdmin):
+    list_display = [
+        "task",
+        "due_date",
+        "is_completed",
     ]
--- todos/models.py
+++ todos/models.py
@@ -4,6 +4,9 @@ class ToDoList(models.Model):
     name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 class ToDoItem(models.Model):
     task = models.CharField(max_length=100)
     due_date = models.DateTimeField(null=True, blank=True)
