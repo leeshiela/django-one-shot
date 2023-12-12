25488e21f6ae6733f6c40d5cf7780a94a436e587
Shiela Lee <leeshiela@gmail.com>
2023-12-12 11:07:34 -0500

    Feature 3 Complete

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class ToDoList(models.Model):
+    name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
